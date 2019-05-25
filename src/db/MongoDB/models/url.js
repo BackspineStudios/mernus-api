22173f3a76b4c7f02c9db2a4c8d995a8f07145db
Gonzalo Trenco <gtrenco@gmail.com>
2019-05-25 20:55:14 +0200

    mongodb class and model loader

---
+++ src/db/MongoDB/models/url.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import { Schema } from 'mongoose';
+
+export const url = (database) => {
+
+  const UrlSchema = new Schema({
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    hash: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    original: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: Array,
+      default: [],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    }
+  }).pre('save', function(next) {
+    const now = new Date();
+    this.updatedAt = now;
+    if (!this.createdAt) {
+      this.createdAt = now;
+    }
+    next();
+  });
+
+  database.model('url', UrlSchema);
+  return database.model('url');
+};
